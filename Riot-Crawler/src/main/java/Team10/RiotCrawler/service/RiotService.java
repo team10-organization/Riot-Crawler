9aaf60ba1783058650dfe3d305b243fee2c205c3
MaSungHo <tonem123@naver.com>
2019-07-11 15:49:11 +0900

    Add getLeaguePositionBySummonerName method

--- Riot-Crawler/src/main/java/Team10/RiotCrawler/service/RiotService.java
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/service/RiotService.java
@@ -1,4 +1,17 @@
 package Team10.RiotCrawler.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RiotService {
+    @Autowired
+    RiotApiClient riotApiClient;
+
+    @Autowired
+    LeaguePositionRepository leaguePositionRepository;
+
+    public LeaguePosition getLeaguePositionBySummonerName(String encryptedSummonerId) {
+        return leaguePositionRepository.findLeaguePosition(encryptedSummonerId);
+    }
 }

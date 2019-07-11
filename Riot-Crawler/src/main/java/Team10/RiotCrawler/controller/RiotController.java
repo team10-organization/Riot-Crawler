6df6f4f9b9453cb728ed6d41250d4f720bc2dee4
mistercle <mistercle@gmail.com>
2019-07-11 15:47:50 +0900

    Create getEncryptedID and getLeagueposition

--- Riot-Crawler/src/main/java/Team10/RiotCrawler/controller/RiotController.java
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/controller/RiotController.java
@@ -1,4 +1,27 @@
 package Team10.RiotCrawler.controller;
 
-public class RiotController {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RiotController
+{
+    @Autowired
+    RiotService riotService;
+
+    @GetMapping("/riot-crawler/encryptedid/{summonername}")
+    public String getEncryptedId(String summonername)
+    {
+        return riotservice.get(summonername);
+    }
+
+
+    @GetMapping("/riot-crawler/leagueposition/{encryptedid}")
+    public LeaguePosition getLeaguePosition(String encryptedid)
+    {
+        return riotservice.getleaguepositionbysummonername(encryptedid);
+    }
+
 }

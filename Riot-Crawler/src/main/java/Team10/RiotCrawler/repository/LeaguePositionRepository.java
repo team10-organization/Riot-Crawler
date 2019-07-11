4716d3e078b9fcd18a3eecc52cb4decb2653f359
mistercle <mistercle@gmail.com>
2019-07-11 16:28:40 +0900

    Create LeaguePositionRepository

---
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/repository/LeaguePositionRepository.java
@@ -0,0 +1,29 @@
+package Team10.RiotCrawler.repository;
+
+import Team10.RiotCrawler.domain.LeaguePosition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public class LeaguePositionRepository
+{
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public void findLeaguePosition(String summonerId)
+    {
+        Query query = Query.query(Criteria.where("summonerId").is(summonerId));
+        return mongoTemplate.findOne(query, LeaguePosition.class);
+    }
+
+    public void insertLeaguePosition(LeaguePosition leaguePosition)
+    {
+        if(findLeaguePosition(leaguePosition.getSummonerId()))
+    }
+
+}

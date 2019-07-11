91ca0a198b273cd113257be4823af3373de5fcf7
mistercle <48170519+mistercle@users.noreply.github.com>
2019-07-11 17:13:56 +0900

    [Pull request ]Repository (#9)
    * Create LeaguePositionRepository
    
    * Create findLeaguePosition method
    
    * Create findLeaguePosition method
    
    * Create insertLeaguePosition method
    
    * Done
    
    * Done

---
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/repository/LeaguePositionRepository.java
@@ -0,0 +1,44 @@
+package Team10.RiotCrawler.repository;
+
+import Team10.RiotCrawler.domain.LeaguePosition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public class LeaguePositionRepository
+{
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public LeaguePosition findLeaguePosition(String summonerId)
+    {
+        Query query = Query.query(Criteria.where("summonerId").is(summonerId));
+        return mongoTemplate.findOne(query, LeaguePosition.class);
+    }
+
+    public void insertLeaguePosition(LeaguePosition leaguePosition)
+    {
+        if(findLeaguePosition(leaguePosition.getSummonerId()) != null)
+        {
+            /*Query query = Query.query(Criteria.where("summonerId").is(leaguePosition.getSummonerId()));
+
+            mongoTemplate.update();*/
+            LeaguePosition temp = new LeaguePosition();
+            temp = mongoTemplate.findOne(Query.query(Criteria.where("summonerId").is(leaguePosition.getSummonerId())), LeaguePosition.class);
+            temp = leaguePosition;
+            mongoTemplate.save(temp);
+
+        }
+        else
+        {
+            mongoTemplate.insert(leaguePosition);
+        }
+    }
+
+}
--- Riot-Crawler/src/main/java/Team10/RiotCrawler/service/RiotService.java
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/service/RiotService.java
@@ -2,6 +2,8 @@ package Team10.RiotCrawler.service;
 
 import Team10.RiotCrawler.api.RiotApiClient;
 import Team10.RiotCrawler.domain.LeaguePosition;
+import Team10.RiotCrawler.domain.SummonerInfo;
+import Team10.RiotCrawler.repository.LeaguePositionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 

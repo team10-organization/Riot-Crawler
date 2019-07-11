86b657591bda86922bfe04a256a1405c66972b8a
tkdgusl94 <tkdgusl94@gmail.com>
2019-07-11 15:58:09 +0900

    create createMongoTemplate()

--- Riot-Crawler/src/main/java/Team10/RiotCrawler/config/MongoConfig.java
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/config/MongoConfig.java
@@ -1,4 +1,15 @@
 package Team10.RiotCrawler.config;
 
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
 public class MongoConfig {
+
+    @Bean
+    public MongoTemplate createMongoTemplate(){
+        return new MongoTemplate(new MongoClient(), "league-position");
+    }
 }

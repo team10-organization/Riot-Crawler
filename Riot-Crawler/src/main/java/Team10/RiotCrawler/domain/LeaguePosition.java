9a4b340784b26df75e141aabc5ce2768ef4508ee
tkdgusl94 <tkdgusl94@gmail.com>
2019-07-11 15:46:49 +0900

    Fill LeaguePosition class

--- Riot-Crawler/src/main/java/Team10/RiotCrawler/domain/LeaguePosition.java
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/domain/LeaguePosition.java
@@ -1,4 +1,20 @@
 package Team10.RiotCrawler.domain;
 
+import lombok.Data;
+
+@Data
 public class LeaguePosition {
+    private String queueType;
+    private String summonerName;
+    private boolean hotStreak;
+    private int wins;
+    private boolean veteran;
+    private int losses;
+    private String rank;
+    private String tier;
+    private boolean inactive;
+    private boolean freshBlood;
+    private String leagueId;
+    private String summonerId;
+    private int leaguePoints;
 }

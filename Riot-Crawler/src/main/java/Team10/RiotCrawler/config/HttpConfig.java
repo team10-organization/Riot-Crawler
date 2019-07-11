963ac1566a3e05782591ecc074f256b1d01930ff
tkdgusl94 <46221990+tkdgusl94@users.noreply.github.com>
2019-07-11 16:20:45 +0900

    domain.LeaguePosition & config package 만듦 (#2)
    * create LeaguePosition.java
    
    * Fill LeaguePosition class
    
    * create HttpConfig.java
    
    * create createRestTemplate()
    
    * create MongoConfig.java
    
    * create createMongoTemplate()
    
    * create summonerInfo
    
    * fill summonerInfo

---
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/config/HttpConfig.java
@@ -0,0 +1,14 @@
+package Team10.RiotCrawler.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class HttpConfig {
+
+    @Bean
+    public RestTemplate createRestTemplate(){
+        return new RestTemplate();
+    }
+}

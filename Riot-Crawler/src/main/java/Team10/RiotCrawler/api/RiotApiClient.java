509217b691f4fce190e5502c46808be40ff8f2f3
tkdgusl94 <tkdgusl94@gmail.com>
2019-07-12 15:25:33 +0900

    return LeaguePosition

--- Riot-Crawler/src/main/java/Team10/RiotCrawler/api/RiotApiClient.java
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/api/RiotApiClient.java
@@ -3,25 +3,30 @@ package Team10.RiotCrawler.api;
 import Team10.RiotCrawler.domain.LeaguePosition;
 import Team10.RiotCrawler.domain.SummonerInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class RiotApiClient {
     private final String api_key = "RGAPI-20c898f3-f039-473f-a8a0-ad9d4e5505ed";
     private final String OpenRiotUrl_getEncryptedId = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={api_key}";
-    private final String OpenRiotUrl_getSummonerInfo = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedId}?api_key={api_key}";
+    private final String OpenRiotUrl_getSummonerInfo = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={api_key}";
+
 
     @Autowired
     RestTemplate restTemplate;
 
-    public SummonerInfo requestEncryptedId(String summonerName){
+    public SummonerInfo requestEncryptedId(String summonerName) {
         return restTemplate.exchange(OpenRiotUrl_getEncryptedId, HttpMethod.GET, null, SummonerInfo.class, summonerName, api_key).getBody();
     }
 
-    public LeaguePosition requestSummonerInfo(String encryptedId){
-        return restTemplate.exchange(OpenRiotUrl_getSummonerInfo, HttpMethod.GET, null, LeaguePosition.class, encryptedId, api_key).getBody();
+    public Set<LeaguePosition> requestSummonerInfo(String encryptedId) {
+        return restTemplate.exchange(OpenRiotUrl_getSummonerInfo, HttpMethod.GET, null, new ParameterizedTypeReference<Set<LeaguePosition>>() {
+        }, encryptedId, api_key).getBody();
     }
-
 }
--- Riot-Crawler/src/main/java/Team10/RiotCrawler/controller/RiotController.java
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/controller/RiotController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
 public class RiotController
 {
@@ -17,9 +19,8 @@ public class RiotController
 
     // LeaguePosition을 return
     @GetMapping("/riot-crawler/leagueposition/{summonerName}")
-    public String getLeaguePosition(@PathVariable String summonerName)
+    public Set<LeaguePosition> getLeaguePosition(@PathVariable String summonerName)
     {
-        //return riotService.getLeaguePositionBySummonerName(summonerName); // LeaguePosition을 리턴
-        return riotService.getEncryptedSummonerId(summonerName); // 테스트로 encryptedId를 리턴.
+        return riotService.getLeaguePositionBySummonerName(summonerName); // LeaguePosition을 리턴
     }
 }

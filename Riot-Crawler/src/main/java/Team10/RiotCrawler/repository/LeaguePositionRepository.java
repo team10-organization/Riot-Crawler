f553cc8ddfd5d08c8387d9196cab2c158a4a4b8a
sehun013 <50852749+sehun013@users.noreply.github.com>
2019-07-12 19:42:04 +0900

    Add db insert method, find method, update method in repository (#16)

--- Riot-Crawler/src/main/java/Team10/RiotCrawler/repository/LeaguePositionRepository.java
+++ Riot-Crawler/src/main/java/Team10/RiotCrawler/repository/LeaguePositionRepository.java
@@ -1,13 +1,17 @@
 package Team10.RiotCrawler.repository;
 
 import Team10.RiotCrawler.domain.LeaguePosition;
+import com.mongodb.DB;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
+
+import java.util.Iterator;
 import java.util.Set;
 
 
@@ -17,31 +21,31 @@ public class LeaguePositionRepository
     @Autowired
     MongoTemplate mongoTemplate;
 
-    public LeaguePosition findLeaguePosition(String summonerId)
+    public LeaguePosition findLeaguePosition(String encryptedId)
     {
-        Query query = Query.query(Criteria.where("summonerId").is(summonerId));
+        Query query = Query.query(Criteria.where("summonerId").is(encryptedId));
+
         return mongoTemplate.findOne(query, LeaguePosition.class);
+
     }
 
     public void insertLeaguePosition(Set<LeaguePosition> leaguePositionSet)
     {
-        /*
-        if(findLeaguePosition(leaguePosition.getSummonerId()) != null)
-        {
-            /*Query query = Query.query(Criteria.where("summonerId").is(leaguePosition.getSummonerId()));
 
-            mongoTemplate.update();
-            LeaguePosition temp = new LeaguePosition();
-            temp = mongoTemplate.findOne(Query.query(Criteria.where("summonerId").is(leaguePosition.getSummonerId())), LeaguePosition.class);
-            temp = leaguePosition;
-            mongoTemplate.save(temp);
+        LeaguePosition leaguePosition = new LeaguePosition();
+        Iterator<LeaguePosition> iter = leaguePositionSet.iterator();
 
-        }
-        else
-        {
-            mongoTemplate.insert(leaguePosition);
-        }*/
         mongoTemplate.insert(leaguePositionSet, LeaguePosition.class);
+
+    }
+
+    public void updateLeaguePosition(LeaguePosition leaguePosition) {
+
+
+        mongoTemplate.update(LeaguePosition.class);
+
+
+
     }
 
 }
